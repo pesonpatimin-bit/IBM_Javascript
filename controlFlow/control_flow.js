68f678749e4d5ef44d21db5a93acaa38cf6ea1af
Peson patimin <peson.patimin@gmail.com>
2025-09-17 06:33:47 -0400

    commit

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,52 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level:", accessLevel);
+
+//userMessage
+let isLoggedIn = true;
+let userMessage;
+
+//check
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+//show message
+console.log("User Message:", userMessage);
+
+//3 Usertyoe
+let userType = "subscriber";
+let userCategory;
+
+//switch usertype
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+//show user type
+console.log("User Category:", userCategory);
